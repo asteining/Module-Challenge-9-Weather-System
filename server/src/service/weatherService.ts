7b42f4b15684e0fe66b6e57be4537c3cb8c4dff8
AJ Steininger <ajs@AJs-MacBook-Pro-3.local>
2024-12-07 14:10:44 -0500

    server-changes

--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
 const API_KEY = '1cd2af220e5d9223c4afa6be38616ba4'; // API key from .env file
-const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather';
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API_KEY}';
 
 const WeatherService = {
   async getWeatherData(cityName: string): Promise<any> {
     try {
-      const geoResponse = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`);
+      const geoResponse = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API_KEY}`);
       if (!geoResponse.data.length) {
         throw new Error('City not found');
       }
