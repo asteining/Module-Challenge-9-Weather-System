d059bb8964c09dcc5268e6d7e883ba436a06cb34
AJ Steininger <ajs@AJs-MacBook-Pro-3.local>
2024-12-13 14:35:57 -0500

    Fix CORS config

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import { Router, type Request, type Response } from 'express';
 import cors from 'cors';
 
 dotenv.config();
@@ -11,8 +12,14 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+const router = Router();
+
+
 
 // Middleware
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 
@@ -21,6 +28,8 @@ app.use(express.json());
 
 // Use the routes
 app.use(routes);
+
+app.options('*', cors());
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
